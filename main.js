b60e5ce43b8872846630d116b0aca902210808b0
coffeegoblins <coffeegoblins@gmail.com>
2014-10-13 17:57:01 -0500

    Post Request Test

--- main.js
+++ main.js
@@ -16,3 +16,23 @@ testRequest.onreadystatechange = function ()
 };
 
 testRequest.send();
+
+var postRequest = new XMLHttpRequest();
+postRequest.open('POST', "https://nodejs-coffeegoblins.rhcloud.com/login");
+postRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+postRequest.onreadystatechange = function ()
+{
+    if (postRequest.readyState === 4)
+    {
+        document.body.innerHTML = "4";
+        if (postRequest.status === 200)
+        {
+            console.log("200");
+        }
+
+        document.body.innerHTML = postRequest.status.toString();
+        document.body.innerHTML += postRequest.responseText;
+    }
+};
+
+postRequest.send('username=' + encodeURIComponent("fawcett") + "&password=" + encodeURIComponent(""));
